a3e43695b579ee4509adad03db27c6776d8a4197
Iris Stream <ngodaisonn@gmail.com>
2022-10-18 12:03:22 +0700

    update vm public key

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -20,7 +20,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   network_interface_ids = [azurerm_network_interface.nic.id]
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCwIYCv5hFjDeY77EexF8+TBi7TFEOnNztD5ERiQ7k1hSL+MgNDorMCKdt1YrHOOyhoY+q2U+xoBgOGVTbhsDYKlFc+BroQtItkqtETlYYI9DSYo/GGM6QA6TVM/uOM6eZXPXPiUaKmm5zAVSAjLXkfdVY1rweJk9Mow+vrHUuc5fsw6yYh/ue3KkUXI+5UlNRioQ1OcjK3gOked1enBjUu+ZDMtIXCWgYBzdd96MAbxcF1f5ay0Iu6izSkuum0/i0c1S0BSr3WdLciuEpGspZQ54gnyt0sD9O0pf+BdfEo1QznQCbEjwXacntElTiZhcc9C/IsDwUWAtSx9bHDpwD8KeT+PGV3f9PqIHbPt12bXho1Ke19h5PeqW6Nsg6WsXcMt0ZKKCdVRHpkfwPwKk3LoVY7dx+OSbDgaPOhjhqLgAZXn4X2sxMRiuKqExlEaUOGCbaeRk5z7xwdTeXD2DSkkqROz/OBpeXBv7uOZpsGScj3wkKrI88SW8VpfsK0p5U= iris@irisstream-pc"
   }
   os_disk {
     caching              = "ReadWrite"
