dc49b94acda8401f287899eb0571723fc9a7af1f
Iris Stream <ngodaisonn@gmail.com>
2022-10-27 17:33:15 +0700

    update selenium test

--- automatedtesting/selenium/UITestSuite.py
+++ automatedtesting/selenium/UITestSuite.py
@@ -42,6 +42,8 @@ if __name__ == "__main__":
     # --uncomment when running in Azure DevOps.
     options = ChromeOptions()
     options.add_argument("--headless") 
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(options=options)
     driver = webdriver.Chrome()
     print ('Browser started successfully. Navigating to the demo page to login.')
